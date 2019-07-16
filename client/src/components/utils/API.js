038f6a43f6f3806cb4cbd2023b6a52e21d31c6a3
KevinL <mastahkevin@gmail.com>
2019-07-16 14:12:36 -0700

    Setting up frontend routing.

---
+++ client/src/components/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+     // gets all books from API
+     getBooks: function(q) {
+          return axios.get('/api/google', {params: {q: 'title' + q}});
+     },
+     // get book with a given id
+     getSavedBooks: function() {
+          return axios.get('/api/books');
+     },
+     // delets the book with the give id
+     deleteBook: function() {
+          return axios.delete('/api/books' + id);
+     },
+     // saved a book to the db
+     saveBook: function() {
+          return axios.post('/api/books', bookData);
+     }
+};
