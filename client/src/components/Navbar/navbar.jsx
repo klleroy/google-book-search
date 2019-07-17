0c73e245d6dc005f84eaf8e7db8415bbf9b70b65
KevinL <mastahkevin@gmail.com>
2019-07-16 18:08:54 -0700

    Completed Navbar.

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -4,11 +4,39 @@ import "./navbar.css";
 
 function Navbar() {
      return (
-          <div className="navbar navbar-expand-lg navbar-light bg-dark">
+          <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                <Link className="navBarText navbar-brand" to="/">Google Books</Link>
-               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarAltMarkup">
+               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                </button>
-          </div>
-     )
+               <div className="collapse navbar-collapse" id="navBarAltMarkup">
+                    <div className="navbar-nav">
+                         <li className="nav-item">
+                              <Link
+                                   to="/" 
+                                   className={
+                                        window.location.pathname === "/" || window.location.pathname === "/search" 
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                   }
+                              >
+                              Search
+                              </Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link
+                                   to="/" 
+                                   className={
+                                        window.location.pathname === "/" || window.location.pathname === "/saved" 
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                   }
+                              >
+                              Saved
+                              </Link>
+                         </li>
+                    </div>
+               </div>
+          </nav>
+     );
 }
