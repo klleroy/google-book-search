d6a83fa8e83b12e2eeacee2fee6deb5b4a6191a6
KevinL <mastahkevin@gmail.com>
2019-07-18 18:52:17 -0700

    Updating to change name of css file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Error from "./pages/Error"
 import Navbar from "./components/Navbar/navbar";
 import Main from "./components/Main/main";
 import Footer from "./components/Footer/footer";
-import "./app.css";
+import "./App.css";
 
 //import books from "";
 
